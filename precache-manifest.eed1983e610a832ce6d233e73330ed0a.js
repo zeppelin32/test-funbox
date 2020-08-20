fe9916bcd962a27afe8e9f89e8a98631a8b2e79a
Dmitry <zeppelin95@yandex.ru>
2020-08-20 12:40:03 +0700

    Updates

---
+++ precache-manifest.eed1983e610a832ce6d233e73330ed0a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f8de9c2c017b3a0ecc9bcdb669207b",
+    "url": "/test-funbox/index.html"
+  },
+  {
+    "revision": "29404362fee060c7054f",
+    "url": "/test-funbox/static/css/main.fb057419.chunk.css"
+  },
+  {
+    "revision": "345f095af19ad6cfa4f5",
+    "url": "/test-funbox/static/js/2.ae3a4dee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-funbox/static/js/2.ae3a4dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29404362fee060c7054f",
+    "url": "/test-funbox/static/js/main.01f3fa77.chunk.js"
+  },
+  {
+    "revision": "7f3f8ed2c5d7bbc26477",
+    "url": "/test-funbox/static/js/runtime-main.2fc311a1.js"
+  },
+  {
+    "revision": "10e5d864d609fae18427ae28922a32a1",
+    "url": "/test-funbox/static/media/Exo2.0-Thin.10e5d864.woff2"
+  },
+  {
+    "revision": "44a35843c5c5c0dc998a0258ea2a7740",
+    "url": "/test-funbox/static/media/Exo2.0-Thin.44a35843.ttf"
+  },
+  {
+    "revision": "d2f778640e2380b9d229bb1cb4d4ac09",
+    "url": "/test-funbox/static/media/Exo2.0-Thin.d2f77864.woff"
+  },
+  {
+    "revision": "05fbcccd789629e303b4dfbf507eb5ca",
+    "url": "/test-funbox/static/media/TrebuchetMS.05fbcccd.woff"
+  },
+  {
+    "revision": "388291dc7a41fe18d4c41ecdb0a19c75",
+    "url": "/test-funbox/static/media/TrebuchetMS.388291dc.woff2"
+  },
+  {
+    "revision": "c8fb4d1637e8b48f8f286e283073aa35",
+    "url": "/test-funbox/static/media/TrebuchetMS.c8fb4d16.ttf"
+  },
+  {
+    "revision": "d212e8bb9b567b6f95b35c65dbd1cbb9",
+    "url": "/test-funbox/static/media/cat.d212e8bb.png"
+  }
+]);
